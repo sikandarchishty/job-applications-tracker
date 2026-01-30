50de245bc94d33bdab6d24bb914df439fb122601
Sikandar Ali <sikandar.chishty@gmail.com>
2026-01-30 16:39:58 +0100

    change style of floating button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -398,7 +398,7 @@ export default function Home() {
       </main>
 
       {/* Floating Add Job Button */}
-      <div className="fixed bottom-10 right-6 md:bottom-6 md:right-6 z-50">
+      <div className="fixed bottom-6 right-6 md:bottom-6 md:right-6 z-50">
         <JobFormDialog
           onCreate={handleCreateJob}
           trigger={
